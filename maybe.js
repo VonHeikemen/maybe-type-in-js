0e17f86b393ebf1c707c92195d7a95324692e265
Heiker Curiel <heikercuriel@gmail.com>
2019-10-28 14:32:46 -0400

    use prettier

--- maybe.js
+++ maybe.js
@@ -1,116 +1,109 @@
 function Maybe(the_thing) {
-  if(the_thing === null 
-     || the_thing === undefined 
-     || the_thing.is_nothing
-  ) {
+  if (the_thing === null || the_thing === undefined || the_thing.is_nothing) {
     return Nothing();
   }
 
-	if(the_thing.is_future || the_thing.is_just) {
-		return the_thing;
-	}
+  if (the_thing.is_future || the_thing.is_just) {
+    return the_thing;
+  }
 
   return Just(the_thing);
 }
 
 function Just(thing) {
-	return {
-		map: fun => Maybe(fun(thing)),
-		and_then: fun => fun(thing),
-		or_else: () => Just(thing),
-		tap: fun => (fun(thing), Just(thing)),
-		unwrap_or: () => thing,
+  return {
+    map: fun => Maybe(fun(thing)),
+    and_then: fun => fun(thing),
+    or_else: () => Just(thing),
+    tap: fun => (fun(thing), Just(thing)),
+    unwrap_or: () => thing,
 
-		filter: predicate_fun => 
-			predicate_fun(thing) 
-				? Maybe(thing)
-				: Nothing(),
+    filter: predicate_fun => (predicate_fun(thing) ? Maybe(thing) : Nothing()),
 
-		is_just: true,
-		is_nothing: false,
-		is_future: false,
-		inspect: () => `Just(${thing})`,
-	};
+    is_just: true,
+    is_nothing: false,
+    is_future: false,
+    inspect: () => `Just(${thing})`
+  };
 }
 
 function Nothing() {
-	return {
-		map: Nothing,
-		and_then: Nothing,
-		or_else: fun => fun(),
-		tap: Nothing,
-		unwrap_or: arg => arg,
+  return {
+    map: Nothing,
+    and_then: Nothing,
+    or_else: fun => fun(),
+    tap: Nothing,
+    unwrap_or: arg => arg,
 
-		filter: Nothing,
+    filter: Nothing,
 
-		is_just: false,
-		is_nothing: true,
-		is_future: false,
-		inspect: () => `Nothing`,
-	};
+    is_just: false,
+    is_nothing: true,
+    is_future: false,
+    inspect: () => `Nothing`
+  };
 }
 
-function Future(promise_thing) { 
-	return {
-		map: fun => Future(promise_thing.then(map_future(fun))),
-		and_then: fun => Future(promise_thing.then(map_future(fun))),
-		or_else: fun => Future(promise_thing.catch(fun)),
-		tap: fun => Future(promise_thing.then(val => (fun(val), val))),
-		unwrap_or: arg => promise_thing.catch(val => arg),
+function Future(promise_thing) {
+  return {
+    map: fun => Future(promise_thing.then(map_future(fun))),
+    and_then: fun => Future(promise_thing.then(map_future(fun))),
+    or_else: fun => Future(promise_thing.catch(fun)),
+    tap: fun => Future(promise_thing.then(val => (fun(val), val))),
+    unwrap_or: arg => promise_thing.catch(val => arg),
 
-		filter: fun => Future(promise_thing.then(filter_future(fun))), 
+    filter: fun => Future(promise_thing.then(filter_future(fun))),
 
-		is_just: false,
-		is_nothing: false,
-		is_future: true,
-		inspect: () => `<Promise>`
-	};
+    is_just: false,
+    is_nothing: false,
+    is_future: true,
+    inspect: () => `<Promise>`
+  };
 }
 
 Future.from_val = function(val) {
-	return Future(Promise.resolve(val));
-}
+  return Future(Promise.resolve(val));
+};
 
 function map_future(fun) {
-	return val => {
-		let promise_content = val;
+  return val => {
+    let promise_content = val;
 
-		if(Maybe(promise_content).is_nothing) {
-			Promise.reject();
-			return;
-		}
+    if (Maybe(promise_content).is_nothing) {
+      Promise.reject();
+      return;
+    }
 
-		if(promise_content.is_just) {
-			promise_content = val.unwrap_or();
-		}
+    if (promise_content.is_just) {
+      promise_content = val.unwrap_or();
+    }
 
-		const result = Maybe(fun(promise_content));
+    const result = Maybe(fun(promise_content));
 
-		if(result.is_just) {
-			return result.unwrap_or();
-		}
+    if (result.is_just) {
+      return result.unwrap_or();
+    }
 
-		return Promise.reject();
-	}
+    return Promise.reject();
+  };
 }
 
 function filter_future(predicate_fun) {
-	return val => {
-		const result = predicate_fun(val);
+  return val => {
+    const result = predicate_fun(val);
 
-		if(result.then) {
-			const return_result = predicate_res => predicate_res 
-				? val
-				: Promise.reject();
-	
-			return result.then(return_result);
-		}
+    if (result.then) {
+      const return_result = predicate_res =>
+        predicate_res ? val : Promise.reject();
 
-		return result ? val : Promise.reject();
-	}
+      return result.then(return_result);
+    }
+
+    return result ? val : Promise.reject();
+  };
 }
 
 module.exports = {
-	Future,
-	Maybe
+  Future,
+  Maybe
 };
--- utils.js
+++ utils.js
@@ -6,30 +6,30 @@ function create_search(data, exact) {
   return input => {
     const word = exact ? input : capitalize(input);
     return Maybe(data[word]);
-  }
+  };
 }
 
 function create_suggest(data) {
-	const fzf = new FuzzySearch(data, [], {caseSensitive: false, sort: true});
+  const fzf = new FuzzySearch(data, [], { caseSensitive: false, sort: true });
 
-	return word => {
-		const matches = fzf.search(word);
-		return Maybe(matches[0]);
-	};
+  return word => {
+    const matches = fzf.search(word);
+    return Maybe(matches[0]);
+  };
 }
 
 async function confirm_word(value) {
-	let answer = await prompts({
-		message: `Did you mean ${value}'`,
-		type: 'confirm',
-		name: 'yes'
-	});
+  let answer = await prompts({
+    message: `Did you mean ${value}'`,
+    type: 'confirm',
+    name: 'yes'
+  });
 
-	return answer.yes;
+  return answer.yes;
 }
 
 function capitalize(str) {
-	return str.charAt(0).toUpperCase() + str.slice(1);
+  return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
 function format(results) {
@@ -37,10 +37,9 @@ function format(results) {
 }
 
 module.exports = {
-	create_search,
-	create_suggest,
-	confirm_word,
-	capitalize,
-	format
-}
-
+  create_search,
+  create_suggest,
+  confirm_word,
+  capitalize,
+  format
+};
